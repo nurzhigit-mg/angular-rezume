873ac0d6102b0740686abdce940167e8dc381bd4
nurzigit-mg <turdumamatovnurzhigit@gmail.com>
2023-06-17 01:30:40 +0600

    rezume

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./components/header.component";
+import {HeroComponents} from "./components/hero.components";
+import {AboutmeCompnent} from "./components/aboutme.compnent";
+import {SmartComponent} from "./components/smart.component";
+import {SkillComponent} from "./components/skill.component";
+import {FutureComponent} from "./components/future.component";
+import {FooterComponent} from "./components/footer.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponents,
+    AboutmeCompnent,
+    SmartComponent,
+    SkillComponent,
+    FutureComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
